f3e72f740f34d0b6441bd8180c0162e3614486a6
maxmudriak <maxim.mudryak@gmail.com>
2017-12-03 20:27:14 +0200

    add mobile version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 ;(function(){
 	"use strict";
+	var menuToggle = document.querySelector('.ba-menu-toggle'),
+		overlay = document.querySelector('.ba-overlay'),
+	        body = document.body;
+
+	    menuToggle.addEventListener('click', function(){
+	        body.classList.toggle('ba-menu-open');
+	    });
+	    overlay.addEventListener('click', function(){
+	        body.classList.toggle('ba-menu-open');
+	    });
 })();
